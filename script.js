987af474811089b86e3b0b6e0def40443bce3163
anthonymxk <anthonyxkeller@gmail.com>
2022-04-15 00:36:40 -0500

    Fix inputSize, added inputSubmit, changed math

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 //total amount of squares in the div
-let size = 16
 let grid = document.querySelector('#grid')
-
-for (let i = 0; i < size; i++) {
-    gridBlock = document.createElement('div');
-    gridBlock.className = 'gridBlock'
-    grid.append(gridBlock)
-    gridBlock.style.width = `${500/Math.sqrt(size)}px`
-    gridBlock.style.height = `${500/Math.sqrt(size)}px`
-}
 
 //clear button & size input
 clear = document.querySelector('#clear')
-clear.addEventListener('click', clearScreen)
 inputSize = document.querySelector('#inputSize')
+inputSubmit = document.querySelector('#inputSubmit')
+
+//event listeners
+clear.addEventListener('click', clearScreen)
+inputSubmit.addEventListener('click', generateGrid)
 
 //clear the screen, hide the clear button, and ask for size input
+function generateGrid() {
+    let size = (document.querySelector('input').value);
+    for (let i = 0; i < (size*size); i++) {
+        gridBlock = document.createElement('div');
+        gridBlock.className = 'gridBlock'
+        grid.append(gridBlock)
+        gridBlock.style.width = `${500/(size)}px`
+        gridBlock.style.height = `${500/(size)}px`
+    }
+    clear.removeAttribute('hidden')
+    inputSize.setAttribute('hidden', 'hidden')
+}
 
 function clearScreen() {
     while (grid.firstChild) {
@@ -24,4 +31,3 @@ function clearScreen() {
     clear.setAttribute('hidden', 'hidden')
     inputSize.removeAttribute('hidden')
 }
-
